e0ce213c0f3123092292d740447099089d6ceffd
darrahts <timothy.s.darrah@vanderbilt.edu>
2019-02-25 15:25:23 -0600

    used ecg signal for examples

--- dft/src/main.cpp
+++ dft/src/main.cpp
@@ -11,13 +11,18 @@
 #include <fstream>
 #include "fourierTransform.hpp"
 
-#define SIG_LEN 320
+#define SIG_LEN 640//320
 
-extern double InputSignal_f32_1kHz_15kHz[320];
-
+//extern double InputSignal_f32_1kHz_15kHz[320];
 //the dft is mirrored, so really only need half
 //just use the full dft and show its reflection about the midpoint
 //TODO make these public class members of the FourierTransform class
+//double real_out[SIG_LEN];
+//double imag_out[SIG_LEN];
+//double mag_out[SIG_LEN];
+//double sig_orig[SIG_LEN];
+
+extern double ecg_raw_sig[640];
 double real_out[SIG_LEN];
 double imag_out[SIG_LEN];
 double mag_out[SIG_LEN];
@@ -39,7 +44,7 @@ void dft_test(FourierTransform* fourier)
 
 	for(int i=0; i< SIG_LEN; i++)
 	{
-		f1<<InputSignal_f32_1kHz_15kHz[i]<<endl;
+		f1<<ecg_raw_sig[i]<<endl;
 		f2<<real_out[i]<<endl;
 		f3<<imag_out[i]<<endl;
 		f4<<mag_out[i]<<endl;
@@ -66,7 +71,11 @@ void inverse_dft_test(FourierTransform* fourier)
 
 int main()
 {
-	FourierTransform* fourier = new FourierTransform(&InputSignal_f32_1kHz_15kHz[0], SIG_LEN);
+//	FourierTransform* fourier = new FourierTransform(&InputSignal_f32_1kHz_15kHz[0], SIG_LEN);
+//	dft_test(fourier);
+//	inverse_dft_test(fourier);
+
+	FourierTransform* fourier = new FourierTransform(&ecg_raw_sig[0], SIG_LEN);
 	dft_test(fourier);
 	inverse_dft_test(fourier);
 
