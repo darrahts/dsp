7091a7c98b5a6d7b4bfb7972588136d96b725066
darrahts <timothy.s.darrah@vanderbilt.edu>
2019-02-25 13:58:05 -0600

    created dft_test function

--- dft/src/main.cpp
+++ dft/src/main.cpp
@@ -14,13 +14,8 @@
 #define SIG_LEN 320
 
 extern double InputSignal_f32_1kHz_15kHz[320];
-//int sig_len = sizeof(InputSignal_f32_1kHz_15kHz)/sizeof(InputSignal_f32_1kHz_15kHz[0]);
 
 //the dft is mirrored, so really only need half
-//double real_out[int(SIG_LEN/2)];
-//double imag_out[int(SIG_LEN/2)];
-//double mag_out[int(SIG_LEN/2)];
-
 //just use the full dft and show its reflection about the midpoint
 double real_out[SIG_LEN];
 double imag_out[SIG_LEN];
@@ -28,12 +23,9 @@ double mag_out[SIG_LEN];
 
 using namespace std;
 
-
-int main()
+void dft_test(FourierTransform* fourier)
 {
 	ofstream f1, f2, f3, f4;
-
-	FourierTransform* fourier = new FourierTransform(&InputSignal_f32_1kHz_15kHz[0], SIG_LEN);
 	fourier->calc_dft(&real_out[0], &imag_out[0]);
 
 	fourier->calc_dft_magnitude(&mag_out[0], &real_out[0], &imag_out[0]);
@@ -49,19 +41,19 @@ int main()
 		f2<<real_out[i]<<endl;
 		f3<<imag_out[i]<<endl;
 		f4<<mag_out[i]<<endl;
-
-//		if(i < SIG_LEN/2)
-//		{
-//			f2<<real_out[i]<<endl;
-//			f3<<imag_out[i]<<endl;
-//			f4<<mag_out[i]<<endl;
-//
-//		}
 	}
+
 	f1.close();
 	f2.close();
 	f3.close();
 	f4.close();
+}
+
+
+int main()
+{
+	FourierTransform* fourier = new FourierTransform(&InputSignal_f32_1kHz_15kHz[0], SIG_LEN);
+	dft_test(fourier);
 
 	return 0;
 }
